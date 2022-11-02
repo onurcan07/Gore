63ec1a4dbb4ca08a25d8b2a3aea9660d55c6f2d3
Onurcan Özer <onurcanozer07@gmail.com>
2022-11-02 21:47:31 +0300

    fix

--- gore/actions/book/bookCrud.go
+++ gore/actions/book/bookCrud.go
@@ -22,7 +22,7 @@ func New(db *gorm.DB) handler {
 }
 
 func (h handler) CreateBook(w http.ResponseWriter, r *http.Request) {
-	// Read to request body
+
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 
@@ -33,23 +33,19 @@ func (h handler) CreateBook(w http.ResponseWriter, r *http.Request) {
 	var book models.Book
 	json.Unmarshal(body, &book)
 
-	// Append to the Books table
 	if result := h.DB.Create(&book); result.Error != nil {
 		fmt.Println(result.Error)
 	}
 
-	// Send a 201 created response
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(book)
 }
 
 func (h handler) DeleteBook(w http.ResponseWriter, r *http.Request) {
-	// Read the dynamic id parameter
+
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
-
-	// Find the book by Id
 
 	var book models.Book
 
@@ -57,7 +53,6 @@ func (h handler) DeleteBook(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(result.Error)
 	}
 
-	// Delete that book
 	h.DB.Delete(&book)
 
 	w.Header().Add("Content-Type", "application/json")
@@ -78,11 +73,10 @@ func (h handler) GetAllBooks(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h handler) GetBook(w http.ResponseWriter, r *http.Request) {
-	// Read dynamic id parameter
+
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 
-	// Find book by Id
 	var book models.Book
 
 	if result := h.DB.First(&book, id); result.Error != nil {
@@ -95,11 +89,10 @@ func (h handler) GetBook(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h handler) UpdateBook(w http.ResponseWriter, r *http.Request) {
-	// Read dynamic id parameter
+
 	vars := mux.Vars(r)
 	id, _ := strconv.Atoi(vars["id"])
 
-	// Read request body
 	defer r.Body.Close()
 	body, err := ioutil.ReadAll(r.Body)
 
